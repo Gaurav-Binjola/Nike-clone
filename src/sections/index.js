36e766a0e9b537a970228eacbf5d7b959ab95b50
gaurav-binjola <gauravsom9266@gmail.com>
2023-09-24 10:29:23 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProducts'
+import Footer from './Footer'
+import Services from './services'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import CustomerReview from './CustomerReview'
+
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReview,
+}
